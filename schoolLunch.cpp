75e03e512ae7d42e582cbd7508234c4571c92f26
Marietjie <270232978@yoobeestudent.ac.nz>
2023-03-21 10:48:21 +1300

    first commit

---
+++ schoolLunch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+void line()
+{
+	std::cout << "................................................................................................................." << std::endl;
+	//a pattern that can be used to make things look neat and make the app more readable
+	//feel free to change pattern
+}
+
+struct login
+{
+	string username;
+	string email;
+	string password;
+};
+
+void loginForm
+{
+
+}
+
+int main()
+{
+	line();
+	std::cout << "School Lunch" << std::end; //Just a heading for that this app will be
+	line();
+
+
+}
