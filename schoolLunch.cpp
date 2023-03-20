125f0840c6706e532ba7a76cab9e1549685636cd
Marietjie <270232978@yoobeestudent.ac.nz>
2023-03-21 11:40:19 +1300

    commit @11:40 21/02

--- schoolLunch.cpp
+++ schoolLunch.cpp
@@ -9,21 +9,53 @@ void line()
 
 struct login
 {
-	string username;
+	string username;  //a struct that can be used for the login form
 	string email;
 	string password;
 };
 
-void loginForm
+void loginForm ()
 {
+	
+	//this will be the login form for exsisting accounts
+}
 
+void newAccount() {
+
+	//This will be questions we ask a new user
+
+}
+
+bool loginGetYN() //
+{
+	char answer;
+
+	std::cout << "Welcome" << sdt::endl; 
+	std::cout << "Do you already have an account with us? (y/n)" << std::endl;
+	std::cin >> answer; //asking the user if they have an account
+
+	if (answer == "y" || answer == "Y")
+	{
+		loginForm();
+	}
+	else if (answer == "n" || answer == "N")
+	{
+		newAccount();
+	}
+	else
+	{
+		std::cout << "Please only enter 'y' or 'n' " << std::endl;
+	}
 }
 
 int main()
 {
+
 	line();
 	std::cout << "School Lunch" << std::end; //Just a heading for that this app will be
 	line();
+
+	loginGetYN();
 
 
 }
