c18cac15f63b47e15934345fec3e10758b945336
MJBytes <127799527+MJBytes@users.noreply.github.com>
2023-03-26 11:24:53 +1300

    11:24 commit

--- HappyMealz.cpp
+++ HappyMealz.cpp
@@ -5,106 +5,95 @@
 #include<istream>
 #include<fstream>
 #include<stdlib.h>
+#include<sstream>
+using namespace std;
+
+void Line() //MJ
+{
+	std::cout << "................................................................................................................." << std::endl;
+}
 
 struct MenuItem {
 	std::string name;
 	double price;
 };
 
-void line() {  //MJ
-	std::cout << "................................................................................................................." << std::endl;
-}
-// a Pattern that can be used to make things look neat and make the app more readable
+bool loginGetYN(); //MJ
 
-bool loginGetYN() { // Richard
-	// Function to ask user if they have an account
-	// and return true if they do and false if they don't
-}
-
-bool checkAccount() {  // Richard
-	bool hasAccount = loginGetYN();
-	if (hasAccount) {
+//bool checkAccount() {  // Richard
+//	bool hasAccount = loginGetYN();
+//	if (hasAccount) {
 		// Code to check if user has existing account
-	}
-	else {
+//	}
+//	else {
 		// Code to create a new account
-	}
-}
+//	}
+//}
 
 void addToCart(std::vector<MenuItem>& cart, const MenuItem& item) {
 	cart.push_back(item);                                              // Richard // this is to add the item to the end of the cart vector
 	std::cout << item.name << " added to cart." << std::endl;
 }
 
-std::vector<MenuItem> cart;             // Richard // to create and empty cart vector
+std::vector<MenuItem> cart; // Richard // to create and empty cart vector
 
 bool ordering = true;
 
-while (ordering) {                      // Richard //  loop while the user is still ordering
-	printMenu(menu);
-	std::cout << "Enter the number of items you would like to order, or enter 0 to check out:" << std::endl;
-	int choice;
-	std::cin >> choice;
-	if (choice > 0 && choice <= menu.size()) {     //Richard // If the user entered a valid menu item number
-		addToCart(cart, menu[choice - 1]);         // Richard // to add the selected product to the cart
-	}
-	else if (choice == 0) {
-		ordering = false;                          // Richard // to set the ordering to false to exit the loop
-	}
-	else {                                        // Richard // // If the user entered an invalid input
-		std::cout << "Invalid choice, please enter a number between 1 and " << menu.size() << std::endl;
-	}
-}
-
-void printCart(const std::vector<MenuItem>&cart) {
+void printCart(const std::vector<MenuItem>& cart) {
 	double total = 0;                               // Richard // this is to initialize the total price to 0
 	std::cout << "cart:" << std::endl;              // Richard // used to display the cart header
-	for (const auto& item : cart) {
+	for (const auto& item : cart)
+	{
 		std::cout << item.name << " -$" << item.price << std::endl;
 		total += item.price;
 	}
-	std::cout << "Total price : $" << total << std::endl;   // Richard // to display the total price
+	std::cout << "Total price : $" << total << std::endl;
 }
 
-void printMenu(const std::vector<MenuItem>&menu) {
+void printMenu(const std::vector<MenuItem>& menu) {
 	std::cout << "Menu:" << std::endl;                 // Richard // Displaying the menu header
 	for (const auto& item : menu) {
 		std::cout << item.name << " -$" << item.price << std::endl;  // Richard // Display the item name and price
 	}
 }
 
-void menuItems() {                       //  Richard // List of Menu items for the user to choose from
+std::vector<MenuItem> menuItems()  //Richard
+{
 	std::vector<MenuItem> menu = {
-		{"Vegan mac n cheese", 6.00},
-		{"Chicken and mayo wrap", 7.50},
-		{"Mince and cheese toasty", 5.50},
-		{"Sushi roll", 7.00},
-		{"Fruit Cup", 4.50},
-		{"Muffin", 5.00},
-		{"Bag of veggie chips", 3.00},
-		{"Vegetarian paella", 8.00},
-		{"Chicken Salad and roll", 6.00},
-		{"Crackers and cheese with apple", 3.00},
+		{"1. Vegan mac n cheese", 6.00},
+		{"2. Chicken and mayo wrap", 7.50},
+		{"3. Mince and cheese toasty", 5.50},
+		{"4. Sushi roll", 7.00},
+		{"5. Fruit Cup", 4.50},
+		{"6. Muffin", 5.00},
+		{"7. Bag of veggie chips", 3.00},
+		{"8. Vegetarian paella", 8.00},
+		{"9. Chicken Salad and roll", 6.00},
+		{"10. Crackers and cheese with apple", 3.00},
 	};
+	return menu;
 }
 
-void printCart(const std::vector<MenuItem>&cart) //Richard
+void Ordering(const std::vector<MenuItem>& menu)
 {
-	// below is to print the contents of the cart
-	double total = 0;
-	std::cout << "cart:" << std::endl;
-	for (const auto& item : cart)
-	{
-		std::cout << item.name << " -$" << item.price << std::endl;
-		total += item.price;  // allowing the product/item price to be added to the total
+	while (ordering)// Richard
+	{ //  loop while the user is still ordering
+		std::cout << std::endl;
+		printMenu(menu);
+		std::cout << std::endl;
+		std::cout << "Enter the number of items you would like to order, or enter 0 to check out:" << std::endl;
+		int choice;
+		std::cin >> choice;
+		if (choice > 0 && choice <= menu.size()) {     //Richard // If the user entered a valid menu item number
+			addToCart(cart, menu[choice - 1]);         // Richard // to add the selected product to the cart
+		}
+		else if (choice == 0) {
+			ordering = false;                          // Richard // to set the ordering to false to exit the loop
+		}
+		else {                                        // Richard // // If the user entered an invalid input
+			std::cout << "Invalid choice, please enter a number between 1 and " << menu.size() << std::endl;
+		}
 	}
-	std::cout << "Total price : $" << total << std::endl;
-}
-
-void addToCart(vector<MenuItem>&cart, const MenuItem & item) //Richard  // here you can add items to the cart from the menu
-{
-	cart.push_back(item);     // this is to add the item to the end of the cart
-	cout << item.name << "The item has been added to your cart." << endl;
 }
 
 struct login //MJ 
@@ -116,14 +105,16 @@ struct login //MJ
 	std::string diet;
 };
 
-void newAccount(login & userLogin);   //MJ
+void newAccount(login& userLogin);   //MJ
 
-void loginForm(login & userLogin) //MJ 
+void loginForm(login& userLogin) //MJ 
 {
-	string name, mail, pass;
+	std::string name, mail, pass;
+	fstream database;
+
 
 	std::cout << "Great! Let's get you logged in" << std::endl;
-	line();
+	Line();
 
 	int attempt = 0, exist = 0;
 
@@ -151,24 +142,33 @@ void loginForm(login & userLogin) //MJ
 		std::cout << "Password: ";
 		std::cin >> userLogin.password;
 
-		ifstream input("database.txt");
+		std::ifstream input("database.txt");
 
-		while (input >> name >> mail >> pass)
+		std::string line;
+
+
+		while (std::getline(input, line))
 		{
+
+			std::stringstream ss(line);
+			ss >> name >> mail >> pass;
+
 			if (name == userLogin.username && mail == userLogin.email && pass == userLogin.password)
 			{
 				exist = 1;
+				break;
 			}
-		}
 
-		input.close();
+		}
 		if (exist == 1)
 		{
-			std::cout << "Hello" << userLogin.username << "The next step is to go to the menu" << std::endl;
-			cin.get();
-			cin.get();
-			cin.get();
-			return;
+			std::cout << std::endl;
+			Line();
+			std::cout << "Hello" << " " << userLogin.username << std::endl;
+			std::cout << std::endl;
+			std::cout << "This will be the Menu for today" << std::endl;
+			Line();
+			Ordering(menuItems());
 		}
 		else
 		{
@@ -179,7 +179,7 @@ void loginForm(login & userLogin) //MJ
 				attempt++;
 				std::cout << std::endl;
 				std::cout << "Login failed. Please try again." << std::endl;
-				line();
+				std::cout << "............................................................" << std::endl;
 			}
 			else
 			{
@@ -216,7 +216,54 @@ void loginForm(login & userLogin) //MJ
 				} while (chooseFollow != 3);
 			}
 		}
+		input.close();
 	} while (attempt != 3);
+}
+
+int main()
+{
+
+	Line();
+	std::cout << "The Happy Mealz" << std::endl; //Just a heading for that this app will be
+	Line();
+
+	loginGetYN();
+
+	return 0;
+}
+
+void newAccount(login& userLogin)   //MJ
+{
+	login newUser;
+	newUser.username;
+	newUser.email;
+	newUser.password;
+	//newUser.allergy;
+	//newUser.diet;
+
+	std::cout << "Let's create a new Account" << std::endl;
+	Line();
+	std::cout << "Enter an Username :";
+	std::cin.ignore();
+	std::getline(std::cin, newUser.username);
+	std::cout << "Add your email address :";
+	std::cin >> newUser.email;
+	std::cout << "Choose your password :";
+	std::cin >> newUser.password;
+	//std::cout << "Do you have any allergies :";
+	//std::cin >> newUser.allergy;
+	//std::cout << "Is there any special diets that we need to consider (lactose intolerance,etc.) : ";
+	//std::cin.ignore();
+	//std::getline(std::cin, newUser.diet);
+
+	std::ofstream regUser("database.txt", std::ios::app);
+	regUser << newUser.username << ' ' << newUser.email << ' ' << newUser.password << ' ' << std::endl;
+	//newUser.allergy << ' ' << newUser.diet <<
+	Line();
+	std::cout << "REGISTERATION WAS SUCCESSFULL\n";
+	Line();
+
+	loginForm(userLogin);
 }
 
 bool loginGetYN() //MJ
@@ -244,50 +291,4 @@ bool loginGetYN() //MJ
 		std::cout << "Please only enter 'y' or 'n' " << std::endl;
 		return false;
 	}
-}
-
-int main()
-{
-
-	line();
-	std::cout << "The Happy Mealz" << std::endl; //Just a heading for that this app will be
-	line();
-
-	loginGetYN();
-
-	return 0;
-}
-
-void newAccount(login & userLogin)   //MJ
-{
-	login newUser;
-	newUser.username;
-	newUser.email;
-	newUser.password;
-	newUser.allergy;
-	newUser.diet;
-
-	std::cout << "Let's create a new Account" << std::endl;
-	line();
-	cout << "Enter an Username :";
-	std::cin.ignore();
-	std::getline(cin, newUser.username);
-	cout << "Add your email address :";
-	cin >> newUser.email;
-	cout << "Choose your password :";
-	cin >> newUser.password;
-	cout << "Do you have any allergies :";
-	cin >> newUser.allergy;
-	cout << "Is there any special diets that we need to consider (lactose intolerance,etc.) : ";
-	std::cin.ignore();
-	std::getline(cin, newUser.diet);
-
-	std::ofstream regUser("database.txt", ios::app);
-	regUser << newUser.username << ' ' << newUser.email << ' ' << newUser.password << ' ' << newUser.allergy << ' ' << newUser.diet << endl;
-
-	line();
-	std::cout << "REGISTERATION WAS SUCCESSFULL\n";
-	line();
-
-	loginForm(userLogin);
 }
